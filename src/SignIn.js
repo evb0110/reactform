f38691f7030404c1a1643144f4851a88ec1c74f4
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2019-06-12 18:44:51 +0300

    all possible

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import './SignIn.css';
+import { isValidEmail, isValidPassword } from './helpers';
 
 class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { validEmail: false };
+    this.onEmailChange = this.onEmailChange.bind(this);
+  }
+
+  onEmailChange(e) {
+    const email = e.target.value;
+    this.setState({ validEmail: isValidEmail(email) });
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+  }
+
+  onPasswordChange(e) {
+    const pass = e.target.value;
+    this.setState({ validPassword: isValidPassword(pass) });
+  }
+
   render() {
+    const isActiveButton = this.state.validEmail && this.state.validPassword;
     return (
       <div className="SignIn">
         <div className="SignIn-header">
@@ -11,14 +30,24 @@ class SignIn extends Component {
         <form className="SignIn-form">
           <div className="email">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="Your company email" />
+            <input
+              type="email"
+              id="email"
+              placeholder="Your company email"
+              onChange={this.onEmailChange}
+            />
           </div>
           <div className="password">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" placeholder="Your password" />
+            <input
+              type="password"
+              id="password"
+              placeholder="Your password"
+              onChange={this.onPasswordChange}
+            />
           </div>
           <div className="submit">
-            <button>Continue</button>
+            <button className={isActiveButton ? 'active' : ''}>Continue</button>
           </div>
         </form>
       </div>
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './SignUp.css';
+import { isValidEmail, isValidPassword } from './helpers';
 
 class SignUp extends Component {
   static defaultProps = {
@@ -21,7 +22,40 @@ class SignUp extends Component {
       'Denmark',
     ],
   };
+
+  constructor(props) {
+    super(props);
+    this.state = { validEmail: false, validPassword: false, phone: '', validPhone: false };
+    this.onEmailChange = this.onEmailChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.onPhoneChange = this.onPhoneChange.bind(this);
+  }
+
+  onEmailChange(e) {
+    const email = e.target.value;
+    this.setState({ validEmail: isValidEmail(email) });
+  }
+  onPasswordChange(e) {
+    const pass = e.target.value;
+    this.setState({ validPassword: isValidPassword(pass) });
+  }
+
+  onPhoneChange(e) {
+    let phone = e.target.value;
+    if (phone.length === 2) {
+      phone += '(';
+    } else if (phone.length === 6) {
+      phone += ')';
+    } else if (phone.length === 10) {
+      phone += '-';
+    } else if (phone.length === 13) {
+      phone += '-';
+    }
+    this.setState({ phone: phone });
+  }
+
   render() {
+    const isActiveButton = this.state.validEmail && this.state.validPassword;
     return (
       <div className="SignUp">
         <div className="SignUp-header">
@@ -40,7 +74,12 @@ class SignUp extends Component {
           </div>
           <div className="email">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="Your company email" />
+            <input
+              type="email"
+              id="email"
+              placeholder="Your company email"
+              onChange={this.onEmailChange}
+            />
           </div>
           <div className="country-phone">
             <div className="country">
@@ -56,15 +95,26 @@ class SignUp extends Component {
             </div>
             <div className="phone">
               <label htmlFor="phone">Phone number</label>
-              <input type="phone" id="phone" placeholder="+7(___)___-__-__" />
+              <input
+                type="phone"
+                id="phone"
+                placeholder="+7(___)___-__-__"
+                onChange={this.onPhoneChange}
+                value={this.state.phone}
+              />
             </div>
           </div>
           <div className="password">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" placeholder="Your password" />
+            <input
+              type="password"
+              id="password"
+              placeholder="Your password"
+              onChange={this.onPasswordChange}
+            />
           </div>
           <div className="submit">
-            <button>Continue</button>
+            <button className={isActiveButton ? 'active' : ''}>Continue</button>
           </div>
         </form>
       </div>
